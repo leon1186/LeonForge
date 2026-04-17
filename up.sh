de58f835b049cc49c22d6ebf75185ce734e3667f
Francis Leon <francisleon1186@yahoo.com>
2026-04-16 20:58:21 -0500

    read up.sh bottom wih user login

--- up.sh
+++ up.sh
@@ -4,3 +4,6 @@ docker exec leonforge_api python manage.py makemigrations
 docker exec leonforge_api python manage.py migrate
 
 docker exec leonforge_api python manage.py shell -c "exec(open('./setup_data.py').read())"
+
+#admin user: leonforge
+#password: admin
